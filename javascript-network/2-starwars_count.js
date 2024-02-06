463a0ea671ea75ca7cf8adf8ea4761e103ccc214
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2024-02-06 11:10:11 +0100

    up

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,26 @@
+// Importing the request module
+const request = require('request');
+
+// Retrieving the API URL from the command line argument
+const apiUrl = process.argv[2];
+
+// Character ID for Wedge Antilles
+const characterId = '18';
+
+// Making a GET request to the Star Wars API endpoint
+request.get(apiUrl, (error, response, body) => {
+    if (error) {
+        // If there's an error, print it
+        console.error(error);
+    } else {
+        // Parsing the response body to extract the films data
+        const filmsData = JSON.parse(body).results;
+
+        // Counting the number of movies where Wedge Antilles is present
+        const moviesWithWedgeAntilles = filmsData.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
+        const count = moviesWithWedgeAntilles.length;
+
+        // Printing the count of movies
+        console.log(count);
+    }
+});
