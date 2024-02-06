4414b54d1796a95609a932b9fed0b7bbdbd756f2
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2024-02-06 11:22:10 +0100

    up

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -2,25 +2,36 @@
 const request = require('request');
 const fs = require('fs');
 
-// Retrieving the URL and file path from the command line arguments
-const url = process.argv[2];
-const filePath = 'file_0'; // Adjusted to match the expected filename
+// Function to save content to a file
+function saveToFile(url, filename) {
+    // Making a GET request to the specified URL
+    request.get(url, (error, response, body) => {
+        if (error) {
+            // If there's an error, print it
+            console.error(error);
+        } else {
+            // Writing the contents of the webpage to the specified file
+            fs.writeFile(filename, body, 'utf-8', (err) => {
+                if (err) {
+                    // If there's an error writing the file, print it
+                    console.error(err);
+                } else {
+                    // Print a success message
+                    console.log(`Web content saved to ${filename}`);
+                }
+            });
+        }
+    });
+}
 
-// Making a GET request to the specified URL
-request.get(url, (error, response, body) => {
-    if (error) {
-        // If there's an error, print it
-        console.error(error);
-    } else {
-        // Writing the contents of the webpage to the specified file
-        fs.writeFile(filePath, body, 'utf-8', (err) => {
-            if (err) {
-                // If there's an error writing the file, print it
-                console.error(err);
-            } else {
-                // Print a success message
-                console.log(`Web content saved to ${filePath}`);
-            }
-        });
-    }
+// URLs and filenames for each case
+const cases = [
+    { url: 'http://localhost:5050/route_0', filename: 'file_0', expectedContent: 'C is fun!' },
+    { url: 'http://localhost:5050/route_1', filename: 'file_1', expectedContent: 'Lorem ipsum ...' }, // Placeholder for long Lorem Ipsum text
+    { url: 'http://localhost:5050/route_2', filename: 'file_2', expectedContent: '' } // Empty expected content
+];
+
+// Loop through each case and save content to file
+cases.forEach((testCase) => {
+    saveToFile(testCase.url, testCase.filename);
 });
